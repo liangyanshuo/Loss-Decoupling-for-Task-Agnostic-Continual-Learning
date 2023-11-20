3d244144054281ac604be1cb140fa2807888ec88
Liang Yanshuo <liangys@LiangdeMacBook-Pro.local>
2023-11-20 22:10:00 +0800

    remove some files

--- Script/run2.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --dataset seq-cifar100 --model lode_esmer --buffer_size 5120 --load_best_args --seed 0 --csv_log &
-CUDA_VISIBLE_DEVICES=5 python main.py --dataset seq-cifar100 --model lode_esmer --buffer_size 5120 --load_best_args --seed 1 --csv_log 
-
-# CUDA_VISIBLE_DEVICES=3 python main.py --dataset seq-cifar100 --model lode_esmer --buffer_size 500 --load_best_args --seed 0 --csv_log &
-# CUDA_VISIBLE_DEVICES=7 python main.py --dataset seq-cifar100 --model lode_esmer --buffer_size 500 --load_best_args --seed 1 --csv_log 
